edd50d59f14996b778dc819b346ec1a94c98d589
Mihai Rizea <>
2018-01-06 19:10:24 +0200

    This is the homework with the autoservice (with increasespeed and decreasespeed methods)

---
+++ Java7RizeaMihai/src/autoservice/Car.java
@@ -0,0 +1,66 @@
+/*
+Class Car
+ */
+package autoservice;
+
+/**
+ *
+ * @author Mihai Rizea
+ */
+public class Car implements Saleable, Rentable{
+    
+    public enum Culoare {RED, YELLOW, BLUE, BLACK, WHITE};
+    public enum Marca {Mercedes, Renault, Dacia};
+    
+    private Culoare carColor = Culoare.BLACK;    
+    private double speed = 0.0;
+    private Marca name = Marca.Dacia;
+    
+    private double rentingPrice = 0;
+
+    public void setRentingPrice(double rentingPrice) {
+        this.rentingPrice = rentingPrice;
+    }
+
+    public void setSalesPrice(double salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+    private double salesPrice = 0;
+    
+    
+    
+    @Override
+    public double getSalesPrice() {
+            return salesPrice;
+    }
+
+    @Override
+    public double getDailyRentingPrice() {
+        return rentingPrice;        
+    }
+    
+    Car(Marca nouaMarca, Culoare nouaCuloare, double nouaViteza, double newRentingPrice, double newSalesPrice){
+    salesPrice = newSalesPrice;
+    rentingPrice = newRentingPrice;
+    carColor = nouaCuloare;
+    name = nouaMarca;
+    speed = nouaViteza;
+    }
+    
+    public void increaseSpeed(short newSpeed){
+if(newSpeed > speed){
+speed = newSpeed;
+}
+else if (newSpeed < speed){
+decreaseSpeed(newSpeed);
+}
+}
+    public void decreaseSpeed(short newSpeed){
+if(newSpeed < speed){
+speed = newSpeed;
+}
+else if (newSpeed < speed){
+increaseSpeed(newSpeed);
+}
+}
+}
